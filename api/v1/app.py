8ae9ec896765623cf997daca81d049bef1e2d3b4
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-26 09:47:39 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from werkzeug.exceptions import NotFound
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(NotFound)
