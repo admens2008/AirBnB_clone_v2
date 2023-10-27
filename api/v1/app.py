7ad78de1da8720995f5259afd4650b9f3a1267e4
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-27 12:15:01 -0700

    don

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lets start the application running APP.py """
+""" Flask Application """
 from api.v1.views import app_views
 from models import storage
 from os import environ
