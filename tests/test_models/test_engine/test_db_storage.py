0d6f74808e1930a883b66334599a4c1a61fb4e3e
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-27 08:20:23 -0700

    test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -80,17 +80,17 @@ class TestDBStorage(unittest.TestCase):
         new_state.save()
         new_user = User(email="bob@foobar.com", password="password")
         new_user.save()
-        self.assertIs(new_state, models.storage.get("State", new_state.id))
-        self.assertIs(None, models.storage.get("State", "blah"))
-        self.assertIs(None, models.storage.get("blah", "blah"))
-        self.assertIs(new_user, models.storage.get("User", new_user.id))
+        self.assertIs(new_state, models.storage.get(State, new_state.id))
+        self.assertIs(None, models.storage.get(State, "blah"))
+        self.assertIs(None, models.storage.get(None, "blah"))
+        self.assertIs(new_user, models.storage.get(User, new_user.id))
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
                      "not testing db storage")
     def test_count(self):
         """test that new adds an object to the database"""
         initial_count = models.storage.count()
-        self.assertEqual(models.storage.count("Blah"), 0)
+        self.assertEqual(models.storage.count(), 0)
         new_state = State(name="Florida")
         new_state.save()
         new_user = User(email="bob@foobar.com", password="password")
