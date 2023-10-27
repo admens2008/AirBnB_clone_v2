beacbc44b40fef19452dbc31ea8be3f3fe00191f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-27 08:52:01 -0700

    jjj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,9 +11,9 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False))
 def status():
-    """ Status of API """
+    """ Status of appii === """
     return jsonify({"status": "OK"})
 
 
