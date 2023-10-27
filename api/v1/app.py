bec672594302f0bc98c84c4b1ea2a9b6c7b0b213
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-27 07:51:33 -0700

    jj

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ test the application  your API  and all the application"""
-import os
-from flask import Flask, jsonify, make_response
-from api.v1.views import app_views
 from models import storage
-from flask_cors import CORS
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+# cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
@@ -25,5 +24,5 @@ def teardown(exc):
 
 if __name__ == '__main__':
     """ main function"""
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', '5000')), threaded=True)
