a481d6446a907f8eaf5099c498a83e6ad4bb63dd
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-27 20:16:55 +0100

    Create app.py

---
+++ api/v1/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Flask Application """
+from api.v1.views import app_views
+from models import storage
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """  close the session connection when done"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    handle error 404
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    """ Ready to lauch the application
+     """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
