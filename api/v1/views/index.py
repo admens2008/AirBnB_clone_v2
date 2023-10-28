11422d74dcc07d24ccb6ad3c25b9da389e7a1ce3
pascallino <luvpascal.ojukwu@yahoo.com>
2023-10-28 04:29:37 -0700

    jj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,11 +7,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
-# from api.v1.views import app_views
+from api.v1.views import app_views
 from flask import jsonify
 from flask import Blueprint
-
-
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
